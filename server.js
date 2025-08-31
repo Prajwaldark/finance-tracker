8256b1118d453a208c860e2f2c4fdd090523da33
Prajwaldark <73590413+Prajwaldark@users.noreply.github.com>
2025-08-31 19:40:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post("/analyze", async (req, res) => {
   `;
 
   const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + process.env.AIzaSyCqaGNkClH81Grqg7fbG2cevDzmwug4Xyg,
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + process.env.gemini api ,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
